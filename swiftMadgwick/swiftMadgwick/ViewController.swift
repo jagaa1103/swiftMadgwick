2b10015b3059d9384f6f70a0e286def92773083c
jagaa <jagaa1103@gmail.com>
2016-11-29 12:10:14 +0100

    added CoreMotion

--- swiftMadgwick/swiftMadgwick/ViewController.swift
+++ swiftMadgwick/swiftMadgwick/ViewController.swift
@@ -7,12 +7,20 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var durationTextField: UITextField!
+    @IBOutlet weak var startButton: UIButton!
+    
+    let motionManager = CMMotionManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        motionManager.deviceMotionUpdateInterval = 0.01
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,80 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
 
+    @IBAction func startCalculation(_ sender: Any) {
+        if self.motionManager.isDeviceMotionActive {
+            return
+        }
+        if durationTextField.text != nil, durationTextField.text != "" {
+            if let duration = durationTextField.text {
+                Timer.scheduledTimer(withTimeInterval: Double(duration)!, repeats: false, block: { _ in
+                    if self.motionManager.isDeviceMotionActive {
+                        self.motionManager.stopDeviceMotionUpdates()
+                    }else if self.motionManager.isGyroActive {
+                        self.motionManager.stopGyroUpdates()
+                    }else if self.motionManager.isAccelerometerActive {
+                        self.motionManager.stopAccelerometerUpdates()
+                    }
+                })
+                collectMotionData()
+            }
+        } else {
+            showAlert(msg: "Insert duration of motion data set for Madgwick calculation")
+        }
+    }
+    
+    
+    func collectMotionData(){
+        motionManager.startDeviceMotionUpdates(to: OperationQueue.main, withHandler: { (data: CMDeviceMotion?, error: Error?) in
+            if error != nil {
+                self.motionManager.stopDeviceMotionUpdates()
+                self.showAlert(msg: error.debugDescription)
+            }else{
+                print("accel: \(data?.gravity)")
+            }
+        })
+    }
+    
+    func checksGyro()->Bool{
+        if motionManager.isGyroAvailable {
+            return true
+        }else{
+            return false
+        }
+    }
+    
+    func gyroDataCollect(){
+        motionManager.gyroUpdateInterval = 0.01
+        motionManager.startGyroUpdates(to: OperationQueue.main, withHandler: { (data: CMGyroData?, error: Error?) in
+            if error != nil {
+                self.motionManager.stopGyroUpdates()
+                self.showAlert(msg: error.debugDescription)
+            }else{
+                
+                print("\(data?.rotationRate.x), \(data?.rotationRate.y), \(data?.rotationRate.z)")
+            }
+        })
+    }
+    
+    func accelDataCollect(){
+        motionManager.accelerometerUpdateInterval = 0.01
+        motionManager.startAccelerometerUpdates(to: OperationQueue.main, withHandler: { (data: CMAccelerometerData?, error: Error?) in
+            if error != nil {
+                self.motionManager.stopAccelerometerUpdates()
+                self.showAlert(msg: error.debugDescription)
+            } else {
+                print("\(data?.acceleration.x), \(data?.acceleration.y), \(data?.acceleration.z)")
+            }
+        })
+    }
+    
+    func showAlert(msg: String){
+        let alert = UIAlertController(title: "Warning", message: msg, preferredStyle: .alert)
+        let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alert.addAction(action)
+        self.present(alert, animated: true, completion: nil)
+    }
 }
 

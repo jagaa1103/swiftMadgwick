55a1daa47af9e86efab52bc7fd9ddecb888e24e4
jagaa <jagaa1103@gmail.com>
2016-11-29 10:46:20 +0100

    init

---
+++ swiftMadgwick/swiftMadgwick/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftMadgwick
+//
+//  Created by Enkhjargal Gansukh on 29/11/2016.
+//  Copyright © 2016 Enkhjargal Gansukh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ swiftMadgwick/swiftMadgwick/swiftMadgwick.swift
@@ -0,0 +1,9 @@
+//
+//  swiftMadgwick.swift
+//  swiftMadgwick
+//
+//  Created by Enkhjargal Gansukh on 29/11/2016.
+//  Copyright © 2016 Enkhjargal Gansukh. All rights reserved.
+//
+
+
